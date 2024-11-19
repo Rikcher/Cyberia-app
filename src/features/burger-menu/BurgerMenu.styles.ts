092df58a10e987cc2f7997ba3a0588fb24d5df7b
Rikcher <nick.richardson.4884@gmail.com>
2024-11-19 12:24:54 +0700

    Made burger menu styles

--- src/features/burger-menu/BurgerMenu.styles.ts
+++ src/features/burger-menu/BurgerMenu.styles.ts
@@ -1,18 +1,14 @@
 import styled from "styled-components";
+import ContactDetails from "../../shared/components/contact-details/ContactDetails";
 
 export const StyledBurgerMenu = styled.div<{$isOpen: boolean}>`
     position: fixed;
     top: 0;
     left: 0;
-    right: 0;
-    bottom: 0;
     width: 100vw;
     height: 100vh;
     background-color: ${({theme}) => theme.colors.primaryShade};
-    padding-top: 8rem; 
-    padding-bottom: 2.5rem; 
-    padding-left: 1.5rem; 
-    padding-right: 1.5rem;
+    padding: 8rem 2.5rem 1.5rem 1.5rem; 
     z-index: 20;
     transition: all 0.3s ease-in-out;
     opacity: ${({ $isOpen }) =>
@@ -21,4 +17,45 @@ export const StyledBurgerMenu = styled.div<{$isOpen: boolean}>`
         $isOpen ? `0` : `-20rem`};
     pointer-events: ${({ $isOpen }) =>
         $isOpen ? `auto` : `none`};
+`
+
+export const StyledBurgerNav = styled.ul`
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: space-between;
+    gap: 1.875rem;
+
+    .nav-link {
+        text-decoration: none;
+        color: ${({theme}) => theme.colors.text};
+        font-size: 1.125rem;
+    }
+`
+
+export const StyledLine = styled.div`
+    height: 2px;
+    width: 100%;
+    border-radius: 999px;
+    background-color: #2B2D3A;
+    margin: 2.75rem 0  2.25rem 0;
+`
+
+export const StyledBurgerMenuHeader = styled.h4`
+    color: ${({ theme }) => theme.colors.gray};
+    font-size: 1.125rem;
+    margin-bottom: 2.75rem;
+`
+
+export const StyledBurgerMenuContactDetails = styled(ContactDetails)`
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: left;
+    gap: 1.25rem;
+
+    li {
+        color: ${({ theme }) => theme.colors.graySecondary};
+        font-size: 1rem;
+    }
 `
--- src/features/burger-menu/BurgerMenu.tsx
+++ src/features/burger-menu/BurgerMenu.tsx
@@ -1,7 +1,31 @@
-import { StyledBurgerMenu } from "./BurgerMenu.styles";
+import {
+    StyledBurgerMenu,
+    StyledBurgerNav,
+    StyledLine,
+    StyledBurgerMenuHeader,
+    StyledBurgerMenuContactDetails,
+} from "./BurgerMenu.styles";
+import Nav from "../../shared/components/nav/Nav";
+import { commonPages } from "../../shared/utils/navigationData";
+import { useTranslation } from "react-i18next";
 
 const BurgerMenu: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
-    return <StyledBurgerMenu $isOpen={isOpen}>BurgerMenu</StyledBurgerMenu>;
+    const { t } = useTranslation();
+
+    return (
+        <StyledBurgerMenu
+            $isOpen={isOpen}
+            className={isOpen ? "no-doc-scroll" : ""}
+        >
+            <StyledBurgerNav>
+                <Nav pages={commonPages} />
+            </StyledBurgerNav>
+            <StyledLine />
+            <StyledBurgerMenuHeader>{t("contacts")}:</StyledBurgerMenuHeader>
+            <StyledBurgerMenuContactDetails />
+            <StyledLine />
+        </StyledBurgerMenu>
+    );
 };
 
 export default BurgerMenu;
